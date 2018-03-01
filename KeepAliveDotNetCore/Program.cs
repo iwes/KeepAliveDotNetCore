a5380b7c1e532b51cd25227f5bc155e71acbfce7
Wes Baehr <wes@abilitybusiness.com>
2018-03-01 11:46:27 -0500

    test program

--- KeepAliveDotNetCore/Program.cs
+++ KeepAliveDotNetCore/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace KeepAliveDotNetCore
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var requests = 0;
+            var failures = 0;
+
+            const string proto = "https";
+            const string host = "speedtest.abilitybusiness.net";
+            const int port = 443;
+
+            var endPoints = Dns.GetHostEntry(host).AddressList.Select(entry => new IPEndPoint(entry, port));
+            var uri = new Uri($"{proto}://{host}:{port}");
+
+            var cts = new CancellationTokenSource();
+
+            Task.Run(() =>
+            {
+                while (!cts.Token.IsCancellationRequested)
+                    try
+                    {
+
+                        {
+                            requests++;
+                            var request = WebRequest.CreateHttp(uri);
+                            request.Timeout = 1000;
+                            request.KeepAlive = true;
+
+                            var response = (HttpWebResponse)request.GetResponse();
+
+                            var connections = IPGlobalProperties
+                                .GetIPGlobalProperties()
+                                .GetActiveTcpConnections()
+                                .Where(c => endPoints.Contains(c.RemoteEndPoint))
+                                .GroupBy(c => c.State)
+                                .Select(g => new { State = g.Key, Count = g.Count(), Ports = g.Select(c => c.LocalEndPoint.Port.ToString()) })
+                                .Select(c => $"{c.State}: {c.Count} ({c.Ports.Aggregate((l, r) => $"{l},{r}")})")
+                                .Aggregate((l, r) => $"{l}; {r}");
+
+                            Console.WriteLine($"Respone [{requests}] ContentLength [{response.ContentLength}] Connections [{connections}]");
+
+                            response.Close();
+                            Task.Delay(500).Wait();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        failures++;
+                        Console.WriteLine(ex);
+                    }
+            }, cts.Token);
+
+            Console.ReadKey();
+            cts.Cancel();
+            Console.WriteLine($"Total Requests [{requests}] Failures [{failures}]");
+            Console.ReadKey();
         }
+
+
     }
 }
---
+++ KeepAliveDotNetCore/README.md
@@ -0,0 +1,13 @@
+﻿
+## Problem
+
+HttpWebRequest in Core does not reuse connections.
+
+
+## Test Core
+
+Execute `dotnet run` and observe output; it will show new connection being created each time.
+
+## Test .NET Framework
+
+Execute `csc Program.cs` then `Program`; it will show connection being re-used.
